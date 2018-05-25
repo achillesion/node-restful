1573b765154d66b94b61d383616ea7fbd3d3d5dc
achillesion <achillesion@gmail.com>
2018-05-26 02:49:28 +0500

    Error Handling
    addressed error handling in main app.js
    Created middle function to handle and show errors.

--- app.js
+++ app.js
@@ -10,5 +10,25 @@ app.use('/products', productRoutes);
 // For Orders route
 app.use('/orders', orderRoutes);
 
+app.use((req,res,next)=>{
+//    create an error object
+    const error =  new Error("Not found")
+//  set up error.status to error code 
+    error.status(404) 
+// pass the error to next
+    next(error)
+    })
+
+app.use((error, req, res, next)=>{
+    // show error staus in responce
+    res.status(error.status||500)
+    // show error message in json responce
+    res.json({
+        error:{
+            message: error.message
+        }
+    })
+})
+
 //Module exports to get file in server.js
 module.exports = app;
